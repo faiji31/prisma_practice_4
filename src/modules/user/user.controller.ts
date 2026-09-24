f98ecc7853a41d8f3059e553b56bcbacea382ec3
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-09-24 20:54:50 +0600

    4th commit

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -28,16 +28,16 @@ const registerUser = catchAsync(async(req:Request,res:Response,next:NextFunction
 const getMyProfile = catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
 
 
-  const {accessToken} = req.cookies
-  console.log(accessToken)
+  // const {accessToken} = req.cookies
+  // console.log(req.user, "my profile")
 
-  const verifiedToken = jwtutlis.verifyToken(accessToken,config.jwt_access_secret)
+  // // const verifiedToken = jwtutlis.verifyToken(accessToken,config.jwt_access_secret)
 
-  if(typeof verifiedToken ==="string"){
-    throw new Error(verifiedToken)
-  }
+  // // if(typeof verifiedToken ==="string"){
+  // //   throw new Error(verifiedToken)
+  // }
 
-  const profile = await userService.getMyProfileIntoDB(verifiedToken.id)
+  const profile = await userService.getMyProfileIntoDB(req.user?.id as string)
 
   sendResponse(res,{
     success:true,
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,15 +1,61 @@
-import { Request, Response, Router } from "express"
+import { NextFunction, Request, Response, Router } from "express"
 import { prisma } from "../../../lib/prisma"
 import config from "../../config"
 import { userController } from "./user.controller"
+import { jwtutlis } from "../../utlis/jwt"
+import { Role } from "../../../generated/prisma/enums"
+import httpstats from "http-status"
 
 
 
 
 const router = Router()
 
+declare global {
+    namespace Express {
+        interface Request{
+            user?:{
+                 email:string,
+                 id:string,
+                 role:Role,
+                 name:string            }
+        }
+    }
+}
+
 
 router.post('/register',userController.registerUser)
-router.get('/me',userController.getMyProfile)
+router.get('/me',(req:Request,res:Response,next:NextFunction)=>{
+  console.log(req.cookies)
+  const {accessToken} = req.cookies
+    console.log(accessToken)
+  
+    const verifiedToken = jwtutlis.verifyToken(accessToken,config.jwt_access_secret)
+
+  
+
+    
+  
+    if(typeof verifiedToken ==="string"){
+      throw new Error(verifiedToken)
+    }
+     const {email,name,role,id} = verifiedToken
+    
+    const requireqdrole = [Role.Admin,Role.Author,Role.User]
+
+    if(!requireqdrole.includes(role)){
+       return  res.status(403).json({
+        success:false,
+        statscode:httpstats.FORBIDDEN,
+        message:"Forbidden!",
+
+       })
+    }
+    req.user ={
+        email,id,role,name
+    }
+  next()
+},userController.getMyProfile)
 
 export const userRoutes = router
+
